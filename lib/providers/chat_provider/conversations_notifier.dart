c9cfab129ee4723044490659ff1cdbd9f45fb260
sgm17 <sergigm78@gmail.com>
2023-02-24 12:59:51 +0100

    action cable dependency

--- lib/providers/chat_provider/conversations_notifier.dart
+++ lib/providers/chat_provider/conversations_notifier.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:action_cable/action_cable.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:platonic/providers/chat_provider/providers.dart';
 import 'package:platonic/domains/chat_repository/chat_repository.dart';
@@ -6,8 +9,33 @@ class ConversationsScrollNotifier
     extends StateNotifier<AsyncValue<List<Conversation>>> {
   final Ref ref;
 
+  StreamSubscription<Message?>? conversationListener;
+
   ConversationsScrollNotifier(this.ref)
       : super(const AsyncValue<List<Conversation>>.loading()) {
+    initialize();
+  }
+
+  void connectToAChannel() {
+    final cable = ActionCable.Connect("ws://10.0.2.2:3000/cable", headers: {
+      "Authorization": "Some Token",
+    }, onConnected: () {
+      print("connected");
+    }, onConnectionLost: () {
+      print("connection lost");
+    }, onCannotConnect: () {
+      print("cannot connect");
+    });
+
+    cable.subscribe("Chat", // either "Chat" and "ChatChannel" is fine
+        channelParams: {"room": "private"},
+        onSubscribed: () {}, // `confirm_subscription` received
+        onDisconnected: () {}, // `disconnect` received
+        onMessage: (Map message) {} // any other message received
+        );
+  }
+
+  Future<void> initialize() async {
     ref
         .watch(chatViewmodelProvider)
         .retrieveConversations()
